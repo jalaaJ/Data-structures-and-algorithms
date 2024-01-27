71a773b680447a3b29c6a769f47901c9a7824e11
jalaaJ <abusalehjalaa1@gmail.com>
2024-01-27 11:27:39 +0200

    trapping water with optimal solution

--- trappingWater.js
+++ trappingWater.js
@@ -20,7 +20,7 @@ function trappingWater(heights) {
 
 console.log(trappingWater([0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]));
 
-console.log("------------------------------");
+console.log("-------------------------------");
 
 function trappingWaterOptimal(heights) {
     let trappedWater = 0;
