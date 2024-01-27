f4cc61d7417849c0e61a7a60f3c14aef8651bb45
jalaaJ <abusalehjalaa1@gmail.com>
2024-01-27 11:13:50 +0200

    most water, trapping water two sum and typedout string all with optimal solutions

---
+++ trappingWater.js
@@ -0,0 +1,54 @@
+function trappingWater(heights) {
+    let trappedWater = 0;
+    for (let p=0; p<heights.length; p++) {
+        let leftP = p, rightP = p, maxLeft = 0, maxRight = 0;
+        while(leftP >= 0) {
+            maxLeft = Math.max(maxLeft, heights[leftP]);
+            leftP--;
+        }
+        while(rightP < heights.length) {
+            maxRight = Math.max(maxRight, heights[rightP]);
+            rightP++;
+        }
+        const currentWater = Math.min(maxLeft, maxRight) - heights[p];
+        if (currentWater >= 0) {
+            trappedWater += currentWater;
+        }
+    }
+    return trappedWater;
+}
+
+console.log(trappingWater([0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]));
+
+console.log("------------------------------");
+
+function trappingWaterOptimal(heights) {
+    let trappedWater = 0;
+    let maxLeft = 0, maxRight = 0;
+    let leftP = 0, rightP = heights.length - 1;
+    while (leftP < rightP) {
+        if (heights[leftP] <= heights[rightP]) {
+            if (heights[leftP] < maxLeft) {
+                const currentWater = maxLeft - heights[leftP];
+                trappedWater += currentWater;
+            }
+            else {
+                maxLeft = heights[leftP];
+            }
+            leftP++;
+        }
+        else {
+            if (heights[rightP] < maxRight) {
+                const currentWater = maxRight - heights[rightP];
+                trappedWater += currentWater;
+            }
+            else {
+                maxRight = heights[rightP];
+            }
+            rightP--;
+        }
+    } 
+    return trappedWater;
+}
+
+console.log(trappingWaterOptimal([0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]));
---
+++ twoSum.js
@@ -0,0 +1,44 @@
+// Brute force solution
+function twoSum(nums, t) {
+    for (let p1 = 0; p1 < nums.length; p1++) {
+        numToFind = t - nums[p1];
+        for (let p2 = p1+1; p2 < nums.length; p2++) {
+            if (numToFind === nums[p2]) {
+                return [p1, p2];
+            }
+        }
+    }
+    return null;
+}
+
+console.log(twoSum([1, 3, 7, 9, 2], 11));
+console.log(twoSum([1, 3, 7, 9, 2], 25));
+console.log(twoSum([1, 6], 7));
+console.log(twoSum([1, 6], 6));
+console.log(twoSum([], 6));
+console.log(twoSum([5], 5));
+
+console.log("------------------------------");
+
+// Optimal solution using a hashmap or an object, to keep track
+// of the nTF and the index of the number that gave us the nTF value.
+function twoSumOptimal(nums, t) {
+    const nTFobj = {};
+    for (let p=0; p<nums.length; p++) {
+        const currentObjVal = nTFobj[nums[p]];
+        if (currentObjVal >= 0) {
+            return [currentObjVal, p];
+        }
+        const ntf = t - nums[p];
+        nTFobj[ntf] = p;
+    }
+    return null;
+}
+
+console.log(twoSumOptimal([1, 3, 7, 9, 2], 11));
+console.log(twoSumOptimal([1, 3, 7, 9, 2], 25));
+console.log(twoSumOptimal([1, 6], 7));
+console.log(twoSumOptimal([1, 6], 6));
+console.log(twoSumOptimal([], 6));
+console.log(twoSumOptimal([5], 5));
+
