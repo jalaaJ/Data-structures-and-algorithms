4e02773215cc00a9eb553a694931f3e8a730cc57
jalaaJ <abusalehjalaa1@gmail.com>
2024-01-29 19:34:25 +0200

    Longest substring without repeating characters, with optimal solution O(N) instead of O(N^2)

--- longestSubstringWithoutRepeatingChars.js
+++ longestSubstringWithoutRepeatingChars.js
@@ -2,7 +2,8 @@
 // Time complexity: O(N^2)
 // Space complexity: O(N) - because the map is getting reset at each iteration ( so it's not O(N^2) )
 
-function longestSunstring(s) {
+function longestSubstring(s) {
+
     if (s.length <= 1) return s.length;
 
     let longest = 0;
@@ -23,7 +24,35 @@ function longestSunstring(s) {
 }
 
 let s = "abcbda";
-console.log(longestSunstring(s));
+console.log(longestSubstring(s));
 
 console.log("-----------------------------------")
 
+function longestSubstringOptimal(s) {
+
+    if (s.length <= 1) return s.length;
+
+    // This hashmap will hold the key as the char we've seen, and the value to be the index of that char 
+    const seen_chars = {};
+
+    let longest = 0;
+    let left = 0;
+    for (let right = 0; right<s.length; right++) {
+        const current_char = s[right];
+        const prevSeen_char = seen_chars[current_char];
+        if (prevSeen_char >= left) {
+            left = prevSeen_char + 1;
+        }
+
+        // Here, in the hashmap, we update the index of the char that we've seen till this point
+        seen_chars[current_char] = right;
+
+        // We can get the length of the current substring by (right - left + 1)
+        longest = Math.max(longest, right - left + 1);
+
+    }
+    return longest;
+}
+
+let s2 = "abcbdaac";
+console.log(longestSubstringOptimal(s2));
