f22858c29e88ae659250266463b3856378508f63
jalaaJ <abusalehjalaa1@gmail.com>
2024-01-31 23:12:22 +0200

    Check if any given string is a Palindrome

---
+++ palindrome.js
@@ -0,0 +1,31 @@
+function isValid_palindrome(s) {
+
+    const alphanumeric_Regex = /[^a-zA-Z0-9]/g;
+    s = s.replace(alphanumeric_Regex, '').toLowerCase();
+
+    if (s.length <= 1) return true;
+
+    if (s.length % 2 === 0) {
+        let left = 0, right = s.length - 1;
+        while (left < right) {
+            if (s[left] !== s[right]) return false;
+            else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+    else {
+        let left = Math.floor(s.length / 2), right = Math.floor(s.length / 2);
+        while (left > 0 && right < s.length) {
+            left--;
+            right++;
+            if (s[left] !== s[right]) return false;
+        }
+        return true;
+    }
+}
+
+const some_string = "A man, a plan, a canal: Panama";
+console.log(isValid_palindrome(some_string));
