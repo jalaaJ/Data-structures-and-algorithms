14a5a9beb8ac8955c54b2f9ebc0076c6a9503f06
jalaaJ <abusalehjalaa1@gmail.com>
2024-02-01 18:06:29 +0200

    Added to check if a string is almost a palindrome if removing one character causes the string to be a palindrome

--- palindrome.js
+++ palindrome.js
@@ -27,5 +27,31 @@ function isValid_palindrome(s) {
     }
 }
 
+// Helper function to skip either one of the characters and continue to check
+function validSubPalindrome(s, left, right) {
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+        else {
+            left++;
+            right--;
+        }
+    }
+    return true;
+}
+
+function isAlmostPalindrome(s) {
+    let left = 0, right = s.length - 1;
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return validSubPalindrome(s, left + 1, right) || validSubPalindrome(s, left, right - 1);
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
 const some_string = "A man, a plan, a canal: Panama";
 console.log(isValid_palindrome(some_string));
