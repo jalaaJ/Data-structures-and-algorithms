ed903a33e6ed2b29e294e623d2ee09fcc8dffada
jalaaJ <abusalehjalaa1@gmail.com>
2024-02-08 22:58:27 +0200

    starting the solution by creating the LinkedList and Node classes

---
+++ m-n-Reversals.js
@@ -0,0 +1,37 @@
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+}
+  
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    printList() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+function m_n_Reversals(head, m, n) {
+    
+}   
