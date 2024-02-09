423da91c33a04fd2175aba1c8e0546072052a237
jalaaJ <abusalehjalaa1@gmail.com>
2024-02-09 21:22:24 +0200

    Finished the question and added the printNode function to print all of the nodes by recieving the head

--- m-n-Reversals.js
+++ m-n-Reversals.js
@@ -32,6 +32,45 @@ class LinkedList {
     }
 }
 
-function m_n_Reversals(head, m, n) {
-    
-}   
+function m_n_reversals(head, m, n) {
+
+    let current_position = 1;
+    let current_node = head;
+    let node_before_reversed = head;
+    while (current_position < m) {
+        node_before_reversed = current_node;
+        current_node = current_node.next;
+        current_position++;
+    }
+    let new_list = null;
+    let reversed_part_tail = current_node;
+    while (current_position >= m && current_position <= n) {
+        const next_node = current_node.next;
+        current_node.next = new_list;
+        new_list = current_node;
+        current_node = next_node;
+        current_position++;
+    }
+    node_before_reversed.next = new_list;
+    reversed_part_tail.next = current_node;
+    if (m > 1) return head;
+    else return new_list;
+}
+
+function printNode(head) {
+    while(head) {
+        console.log(head);
+        head = head.next;
+    }
+}
+
+let list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+list.append(5);
+list.printList();
+const reversed = m_n_reversals(list.head, 2, 4);
+console.log("------------------------");
+printNode(reversed);
